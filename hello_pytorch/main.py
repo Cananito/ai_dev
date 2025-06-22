a6bfd2f44ff34433844b856ca443dddabbf3f600
Rogelio Gudino <cananito@gmail.com>
2025-06-22 09:09:03 -0700

    Makes hello_pytorch run on the GPU selecting between CUDA and Metal depending on what's available.

--- hello_pytorch/main.py
+++ hello_pytorch/main.py
@@ -1,4 +1,13 @@
 import torch
 
-tensor = torch.tensor([[1, 2], [3, 4], [5, 6]])
+device = None
+if torch.cuda.is_available():
+    print("Using the CUDA backend.")
+    device = torch.device("cuda")
+elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+    print("Using the Metal backend.")
+    device = torch.device("mps")
+
+tensor = torch.rand(2, 3, device=device)
+
 print(tensor)
